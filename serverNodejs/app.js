b6ca9e884ac1bf4da392fae739cab6c49275a9a5
naseerfetrat <naseer.fetrat@gmail.com>
2020-10-11 08:08:37 +0430

    First Commit

---
+++ serverNodejs/app.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line
+/* eslint-disable */
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const config = require("./config/config");
+const app = express();
+const mongoose = require('mongoose');
+const user = require('./models/User')
+const song = require('./models/Songs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("combined"));
+
+
+require("./routes/routes")(app);
+
+if(process.env.NODE_ENV === 'prodection'){
+	//static folder
+	app.use(express.static("public"));
+	//handle SPA
+	app.get(/.*/,(req , res)=>res.sendFile(__dirname + '/public/index.html'));
+
+}
+
+// db config
+const url = config.db_url;
+
+mongoose.connect(url,{ useNewUrlParser: true ,useUnifiedTopology: true })
+    .then(() => {
+        console.log("connected to mongodb")
+        app.listen(config.port, () => {
+            console.log(`the server is running on port ${config.port}`);
+        })
+
+    }).catch(err => { console.log(err)});
+
